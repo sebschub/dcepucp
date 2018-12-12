0e799f6015f4b359ddc18079f573932642f46d53
Sebastian Schubert <sebastian.schubert@geo.hu-berlin.de>
2018-12-12 22:14:35 +0100

    Update generation from lcz

--- R/lcz.R
+++ R/lcz.R
@@ -1,8 +1,8 @@
 validate_lcz_parameter <- function(name_class, name_property, value_property) {
-  if ((!is.na(dcepucp:::lcz_ranges[[name_class]][[name_property]]$max) &
-       value_property > dcepucp:::lcz_ranges[[name_class]][[name_property]]$max) |
-      (!is.na(dcepucp:::lcz_ranges[[name_class]][[name_property]]$min) &
-       value_property < dcepucp:::lcz_ranges[[name_class]][[name_property]]$min) ) {
+  if ((!is.na(lcz_ranges[[name_class]][[name_property]]$max) &
+       value_property > lcz_ranges[[name_class]][[name_property]]$max) |
+      (!is.na(lcz_ranges[[name_class]][[name_property]]$min) &
+       value_property < lcz_ranges[[name_class]][[name_property]]$min) ) {
 
     warning(paste0(name_property, " in class \"", name_class, "\" is out of range."))
   }
@@ -23,13 +23,13 @@ validate_lcz <- function(lcz) {
 
   stopifnot(class(lcz) == "lcz")
 
-  for (i_class in seq_along(dcepucp:::lcz_ranges)) {
-    name_class <- names(dcepucp:::lcz_ranges)[i_class]
+  for (i_class in seq_along(lcz_ranges)) {
+    name_class <- names(lcz_ranges)[i_class]
 
     # all properties of each class
-    for (i_property in seq_along(dcepucp:::lcz_ranges[[name_class]])) {
+    for (i_property in seq_along(lcz_ranges[[name_class]])) {
 
-      name_property <- names(dcepucp:::lcz_ranges[[name_class]])[i_property]
+      name_property <- names(lcz_ranges[[name_class]])[i_property]
       validate_lcz_parameter(name_class, name_property, lcz[[name_class]][[name_property]])
 
     }
@@ -53,26 +53,26 @@ print.lcz <- function(x, ...) {
   stopifnot(class(x) == "lcz")
 
   # all LCZ classes
-  for (i_class in seq_along(dcepucp:::lcz_ranges)) {
+  for (i_class in seq_along(lcz_ranges)) {
 
-    name_class <- names(dcepucp:::lcz_ranges)[i_class]
+    name_class <- names(lcz_ranges)[i_class]
     cat(name_class)
     cat("\n")
 
     # all properties of each class
-    for (i_property in seq_along(dcepucp:::lcz_ranges[[i_class]])) {
+    for (i_property in seq_along(lcz_ranges[[i_class]])) {
 
-      name_property <- names(dcepucp:::lcz_ranges[[i_class]])[i_property]
+      name_property <- names(lcz_ranges[[i_class]])[i_property]
       cat(paste0("  ", name_property, ": ", x[[name_class]][[name_property]], " "))
 
       # print ranges
-      if (is.na(dcepucp:::lcz_ranges[[i_class]][[i_property]]$min)) {
-        cat(paste0("(< ", dcepucp:::lcz_ranges[[i_class]][[i_property]]$max, ")"))
-      } else if (is.na(dcepucp:::lcz_ranges[[i_class]][[i_property]]$max)) {
-        cat(paste0("(> ", dcepucp:::lcz_ranges[[i_class]][[i_property]]$min, ")"))
+      if (is.na(lcz_ranges[[i_class]][[i_property]]$min)) {
+        cat(paste0("(< ", lcz_ranges[[i_class]][[i_property]]$max, ")"))
+      } else if (is.na(lcz_ranges[[i_class]][[i_property]]$max)) {
+        cat(paste0("(> ", lcz_ranges[[i_class]][[i_property]]$min, ")"))
       } else {
-        cat(paste0("(", dcepucp:::lcz_ranges[[i_class]][[i_property]]$min,
-                   " - ", dcepucp:::lcz_ranges[[i_class]][[i_property]]$max, ")"))
+        cat(paste0("(", lcz_ranges[[i_class]][[i_property]]$min,
+                   " - ", lcz_ranges[[i_class]][[i_property]]$max, ")"))
       }
       cat("\n")
     }
@@ -121,14 +121,67 @@ set_lcz_parameter <- function(lcz, lcz_name, h2w = NULL, fr_build = NULL, fr_str
   lcz
 }
 
+
+upar_lcz_dominant_class <- function(grid, lcz, dominant_class, fr_urb) {
+
+  fr_uclass <- array(1./grid$n_uclass, dim = c(grid$ie_tot, grid$je_tot, grid$n_uclass))
+  fr_udir <- array(1./grid$n_udir, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
+  fr_roof <- array(0., dim = c(grid$ie_tot, grid$je_tot, grid$ke_uhl + 1, grid$n_udir, grid$n_uclass))
+
+  w_street <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
+  w_build <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
+
+  h2w <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
+  fr_build <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
+  fr_street <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
+  height <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
+
+  for (i in 1:grid$ie_tot) {
+    for (j in 1:grid$je_tot) {
+
+      if (!is.na(dominant_class[i,j])) {
+
+        h2w[i, j] <- lcz[[ dominant_class[i,j] ]]$h2w
+        fr_build[i, j] <- lcz[[ dominant_class[i,j] ]]$fr_build
+        fr_street[i, j] <- lcz[[ dominant_class[i,j] ]]$fr_street
+        height[i, j] <- lcz[[ dominant_class[i,j] ]]$height
+
+        if (lcz[[ dominant_class[i,j] ]]$height > max(grid$hhl_uhl))
+          stop("Grid too low for dominant class \"", dominant_class[i,j], "\".")
+
+        height_index <- which.min(abs(grid$hhl_uhl - lcz[[ dominant_class[i,j] ]]$height))
+
+        fr_roof[i, j, height_index, , 1] <- 1.
+      }
+    }
+  }
+
+  for (id in 1:grid$n_udir) {
+    w_street[ , , id, 1] <- height / h2w
+    w_build[ , , id, 1] <- fr_build / fr_street * w_street[ , , id, 1]
+  }
+
+  upar(grid, fr_urb, fr_uclass, fr_udir, fr_roof, w_street, w_build)
+
+}
+
+
 #' Generate DCEP paramters from LCZ maps
 #'
-#' @param grid Object of class \code{ugrid}.
-#' @param lcz Object of class \code{lcz}.
-#' @param maps LCZ maps
-#' @param mode Mode
+#' The parameters can be derived either from a map of the dominant class or a
+#' list of fractions of local climate zones.
+#'
+#' @param grid Grid specifications, object of class \code{ugrid}.
+#' @param lcz LCZ parameters, object of class \code{lcz}.
+#' @param dominant_class Map of dominant class of each grid cell.
+#' @param class_fractions List of maps, each describing the fraction of urban
+#'   local climate zones.
 #'
 #' @return An object of class \code{upar}.
+#' @details Either an argument for \code{dominent_class} or for
+#'   \code{class_fraction} has to be supplied. Currently, only one urban class
+#'   is supported.
+#'
 #' @export
 #'
 #' @examples
@@ -141,73 +194,57 @@ set_lcz_parameter <- function(lcz, lcz_name, h2w = NULL, fr_build = NULL, fr_str
 #' n_uclass = 1,
 #' n_udir = 4, angle_udir = c(-45., 0., 45., 90.))
 #'
-#' up <- upar_lcz(berlin_grid, lcz_default, maps = list("Open low-rise" = array(1, c(4,5))))
-upar_lcz <- function(grid, lcz, maps, mode = "dominant_class") {
+#' up <- upar_lcz(berlin_grid, lcz_default, class_fractions = list("Open low-rise" = array(1, c(4,5))))
+upar_lcz <- function(grid, lcz, dominant_class = NULL, class_fractions = NULL) {
 
   stopifnot(class(grid) == "ugrid")
+  stopifnot(grid$n_uclass == 1)
+  stopifnot(xor(is.null(dominant_class), is.null(class_fractions)))
 
-  if (class(maps) == "list") {
+  fr_urb <- array(0., dim = c(grid$ie_tot, grid$je_tot))
+
+  if (!is.null(class_fractions)) {
+    stopifnot(class(class_fractions) == "list")
     # check if dimensions are ok
-    for (i_lcz in seq_along(maps)) {
+    for (i_lcz in seq_along(class_fractions)) {
     #  stopifnot_fraction_or_na(lcz_maps[[i_lcz]])
-      stopifnot(dim(maps[[i_lcz]]) == c(grid$ie_tot, grid$je_tot))
+      stopifnot(dim(class_fractions[[i_lcz]]) == c(grid$ie_tot, grid$je_tot))
     }
-    maps <- simplify2array(maps)
-  }
+    class_fractions <- simplify2array(class_fractions)
 
-  stopifnot(all( apply(maps, c(1,2), mean, na.rm = TRUE) <= 1.))
+    stopifnot(all( apply(class_fractions, c(1,2), mean, na.rm = TRUE) <= 1.))
 
-  stopifnot(mode %in% c("dominant_class", "classes"))
+    lcz_names <- dimnames(class_fractions)[[3]]
 
-  if (mode == "dominant_class") {
-    lcz_names <- dimnames(maps)[[3]]
-
-    fr_urb <- array(0., dim = c(grid$ie_tot, grid$je_tot))
-    fr_uclass <- array(1./grid$n_uclass, dim = c(grid$ie_tot, grid$je_tot, grid$n_uclass))
-    fr_udir <- array(1./grid$n_udir, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
-    fr_roof <- array(0., dim = c(grid$ie_tot, grid$je_tot, grid$ke_uhl + 1, grid$n_udir, grid$n_uclass))
-
-    w_street <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
-    w_build <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot, grid$n_udir, grid$n_uclass))
-
-    h2w <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
-    fr_build <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
-    fr_street <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
-    height <- array(NA_real_, dim = c(grid$ie_tot, grid$je_tot))
+    dominant_class <- array(NA_integer_, dim = c(grid$ie_tot, grid$je_tot))
 
     for (i in 1:grid$ie_tot) {
       for (j in 1:grid$je_tot) {
 
+        # fr_urb based on average of all classes
         for (lcz_i in seq_along(lcz_names)) {
-          if (!is.na(maps[i,j, lcz_i])) {
+          if (!is.na(class_fractions[i,j, lcz_i])) {
             fr_urb[i,j] <- fr_urb[i,j] +
-              maps[i,j, lcz_i]*(lcz[[lcz_names[lcz_i]]]$fr_build + lcz[[lcz_names[lcz_i]]]$fr_street)
+              class_fractions[i,j, lcz_i]*(lcz[[lcz_names[lcz_i]]]$fr_build + lcz[[lcz_names[lcz_i]]]$fr_street)
           }
         }
-
-        dominant_class <- lcz_names[which.max(maps[i, j, ])]
-
-        h2w[i, j] <- lcz[[dominant_class]]$h2w
-        fr_build[i, j] <- lcz[[dominant_class]]$fr_build
-        fr_street[i, j] <- lcz[[dominant_class]]$fr_street
-        height[i, j] <- lcz[[dominant_class]]$height
-
-        if (lcz[[dominant_class]]$height > max(grid$hhl_uhl))
-          stop("Grid too low for dominant class \"", dominant_class, "\".")
-
-        height_index <- which.min(abs(grid$hhl_uhl - lcz[[dominant_class]]$height))
-
-        fr_roof[i, j, height_index, , 1] <- 1.
+        # derive dominant class
+        dominant_class[i, j] <- which(names(lcz) == lcz_names[which.max(class_fractions[i, j, ])])
       }
     }
+  } else if (!is.null(class_fractions)) {
 
-    for (id in 1:grid$n_udir) {
-      w_street[ , , id, 1] <- height / h2w
-      w_build[ , , id, 1] <- fr_build / fr_street * w_street[ , , id, 1]
+    for (i in 1:grid$ie_tot) {
+      for (j in 1:grid$je_tot) {
+        # fr_urb based on given class
+        if (!is.na(class[i,j])) {
+          fr_urb[i,j] <- lcz[[ class[i,j] ]]$fr_build + lcz[[ class[i,j] ]]$fr_street
+        }
+      }
     }
-
-    upar(grid, fr_urb, fr_uclass, fr_udir, fr_roof, w_street, w_build)
-
   }
+
+  upar_lcz_dominant_class(grid = grid, lcz = lcz,
+                          dominant_class = dominant_class, fr_urb = fr_urb)
 
 }
